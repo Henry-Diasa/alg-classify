fe2adfdc1aadb3228114d13a4dc1b30bd7983133
fuqiang1 <fuqiang1@sensetime,com>
2021-12-05 22:13:23 +0800

    1

---
+++ LinkedList/237.js
@@ -0,0 +1,3 @@
+// https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
+
+// 无法获取到 待删除节点的前一个节点  所以只能改变值得方式
---
+++ LinkedList/445.js
@@ -0,0 +1,3 @@
+// https://leetcode-cn.com/problems/add-two-numbers-ii/
+
+// 栈结构的应用 和 前插链表
